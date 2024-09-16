ac5c1026bc0690495580f235c75cc3a10012c62b
Gabor Heja <gheja@ubuntu1.w830g8.git.stdio.hu>
2024-09-16 03:28:12 +0200

    Updated the swing acceleration calculation - not the best but it is good enough

--- src/scenes/main_level.gd
+++ src/scenes/main_level.gd
@@ -1,63 +1,53 @@
 extends Node3D
 
 const SLOW_DOWN_AMOUNT_PER_SEC = 0.2
-const SLOW_DOWN_AMOUNT_PER_SWING = 0.3
 const HEIGHT_INCREASE_PER_SEC = 50.0
 const SWING_MULTIPLIER = 1.5
-const MIN_HEIGHT = 0.5
-const MAX_HEIGHT = 160.0
+const MIN_MAX_ANGLE = 0.5
+const MAX_MAX_ANGLE = 160.0
 
-var t = 0.0
-
-# this is not really height but... rather the maximum angle which the swing will swing
-var height = 0.0
+var t = -10.0
+var max_angle = 100.0
+var next_max_angle
+var target_max_angle
 
 func _ready() -> void:
-	height = 100.0
-	next_height = height
-	target_height = height
+	max_angle = 50.0
+	next_max_angle = max_angle
+	target_max_angle = max_angle
 
 var a = 0.0
-var last_a = 0.0
-
-var next_height = 0.0
-var target_height = 0.0
+var last_a = 0
 
 func _process(delta: float) -> void:
+	var sign_a
 	
 	t += delta
 	
-	var a = cos(t * SWING_MULTIPLIER) * height
-	# var a = 
+	a = cos(t) * max_angle
+	sign_a = sign(a - last_a)
 	
-	# if just swung by the middle point
-	if last_a < 0 and a > 0:
-		next_height = clamp(next_height, MIN_HEIGHT, MAX_HEIGHT)
-		
-		target_height = next_height
-		
-		next_height = next_height * (1.0 - SLOW_DOWN_AMOUNT_PER_SWING)
+	max_angle = lerp(max_angle, target_max_angle, 0.0035)
 	
-	# height = height + (target_height)
-	height = lerp(height, target_height, 0.1)
+	next_max_angle = next_max_angle * (1 - SLOW_DOWN_AMOUNT_PER_SEC * delta)
+	next_max_angle = clamp(next_max_angle, MIN_MAX_ANGLE, MAX_MAX_ANGLE)
+	
+	if (last_a < 0 and a > 0) or (last_a > 0 and a < 0):
+		target_max_angle = next_max_angle
 	
 	$LevelObjects/SwingThing.rotation_degrees.x = a
 	
-	var change_speed = a - last_a
-	
-	if change_speed > 0:
+	if Input.get_action_strength("action_go") > 0.5:
+		next_max_angle += HEIGHT_INCREASE_PER_SEC * delta * sign_a
 		$LevelObjects/PlayerCharacter/AnimationPlayer.play("sit_forward")
 	else:
 		$LevelObjects/PlayerCharacter/AnimationPlayer.play("sit_backward")
-	
-	if Input.get_action_strength("action_go") > 0.5:
-		next_height += HEIGHT_INCREASE_PER_SEC * delta * sign(change_speed)
 	
 	$LevelObjects/PlayerCharacter.global_position = $LevelObjects/SwingThing/PivotStart/Construct/Marker3D.global_position
 	$LevelObjects/PlayerCharacter.rotation_degrees.x = $LevelObjects/SwingThing.rotation_degrees.x
 	
 	$Camera3D.look_at($LevelObjects/PlayerCharacter/CameraMarker.global_position)
 	
-	print([ height, change_speed, a ])
+	# print([a, last_a, max_angle, target_max_angle, next_max_angle, sign_a ])
 	
 	last_a = a
